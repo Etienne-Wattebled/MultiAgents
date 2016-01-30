9ee1ecc3597998753cdd62225387a18e35d26b59
LorneMalvoo <delaeter.jb@gmail.com>
2016-01-30 15:46:54 +0100

    Ajout classes Avatar et Chasseur

---
+++ src/main/java/modeles/agents/Avatar.java
@@ -0,0 +1,13 @@
+package modeles.agents;
+
+import simulateurs.Simulateur;
+
+public class Avatar extends Agent{
+	public Avatar(Simulateur simulateur, int posX, int posY, Direction direction) {
+		super(simulateur,posX,posY,direction);
+	}
+	
+	public void interagir(){
+		
+	}
+}
---
+++ src/main/java/modeles/agents/Chasseur.java
@@ -0,0 +1,13 @@
+package modeles.agents;
+
+import simulateurs.Simulateur;
+
+public class Chasseur extends Agent{
+	public Chasseur(Simulateur simulateur, int posX, int posY, Direction direction) {
+		super(simulateur,posX,posY,direction);
+	}
+	
+	public void interagir(){
+		
+	}
+}
